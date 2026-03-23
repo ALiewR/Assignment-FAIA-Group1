458d616aaffb9ac3f10529b920573163ac1c74f4
ALiewR <arielliewrui@gmail.com>
2026-03-23 13:28:41 +0800

    added temp BattleEngine use in main
    should come after LoadingScreen init BattleContext

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    // TEMP
+    Printer printer = new Printer();
+    BattleContext battleContext = new BattleContext();
+    BattleEngine battleEngine = new BattleEngine(battleContext, printer);
 
+    // player won, exit game
+    while (!battleEngine.executeBattle()) {
+        // TEMP
+        printer.printLine("You lost!.", true);
+        return;
+        //battleEngine.executeBattle();
+    }
+    // TEMP
+    printer.printLine("You win!", true);
 }
