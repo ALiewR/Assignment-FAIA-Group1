93410cd8bb97b192f79d065192bfc90bc7718eac
ALiewR <arielliewrui@gmail.com>
2026-03-23 13:24:53 +0800

    Create UI.java

---
+++ src/UI.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public abstract class UI {
+    private Scanner scanner = new Scanner(System.in);
+    private Printer printer;
+
+    public UI(Printer printer) {
+        this.printer = printer;
+    }
+
+    public void displayMessage(String msg) {
+        checkPrinter();
+        printer.print(msg);
+    }
+    public void displayLineMessage(String msg) {
+        checkPrinter();
+        printer.printLine(msg);
+    }
+    public void displayMessage(String msg, boolean isBold) {
+        checkPrinter();
+        printer.print(msg, isBold);
+    }
+    public void displayLineMessage(String msg, boolean isBold) {
+        checkPrinter();
+        printer.printLine(msg, isBold);
+    }
+    private void checkPrinter() {
+        if (printer == null) printer = new Printer(); // use default printer if no printer assigned
+    }
+    public int getUIInt() {
+        return scanner.nextInt();
+    }
+}
