c1db170580b87fce705e1bc27327b97faa9ffe26
Bharat_12345 <devcricaggarwal29@gmail.com>
2026-04-06 14:17:41 +0800

    edited

--- src/Item.java
+++ src/Item.java
@@ -1,17 +1,34 @@
-public class Item {
+public abstract class Item {
     boolean isUsed = false;
-    public String name = "item";
+    protected String name = "item";
+    protected boolean used;
     public String description = "Power Stone does not affect cooldown";
     public int currentDurationLeft = 0;
     public final int maxDuration = 2;
     public ITEM_TYPE itemType = ITEM_TYPE.SMOKE_BOMB;
+    public Item(String name)
+    {
+        this.name=name;
+        this.used=false;
+    }
+    public String getName() {
+        return name;
+    }
+    public boolean isUsed() {
+        return used;
+        
+    }
+    public void markAsUsed() {
+            used=true;
+    }
+    public void resetItem() {
+            used=false;
+    }
+    public abstract void use(Combatant user, Combatant target);
     public void use() { isUsed = true; currentDurationLeft = maxDuration; }
     public void resetUse() { isUsed = false; currentDurationLeft = 0; }
     public boolean getIsUsed() { return isUsed; }
     public void depleteDuration() {
         if (isUsed) currentDurationLeft--;
-    }
-    public Item(String name) {
-        this.name = name;
     }
 }
