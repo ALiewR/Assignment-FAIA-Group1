becb0f64e52b6a0dd55b2b30d14305e232b714e3
D3VYXNSH <devyansh002@e.ntu.edu.sg>
2026-04-06 14:11:34 +0800

    improved description

--- src/Defend.java
+++ src/Defend.java
@@ -2,7 +2,7 @@ import java.util.List;
 public class Defend extends Action{
     public Defend()
     {
-        super("Defend","Increases defence",ACTION_TYPE.ATTACK,1);
+        super("Defend","Increases defence for the current and next round",ACTION_TYPE.ATTACK,1);
         this.targetType=TARGET_TYPE.SELF;
     }
     @Override
