8a9a99014a816601b443beefb558d157b81c7547
D3VYXNSH <devyansh002@e.ntu.edu.sg>
2026-04-09 23:02:07 +0800

    added overloaded execute to control cooldown resetting

--- src/SpecialSkill.java
+++ src/SpecialSkill.java
@@ -1,8 +1,17 @@
+import java.util.List;
 public abstract class SpecialSkill extends Action 
 {
     public SpecialSkill(String name)
     {
         super(name,"Special Skill",ACTION_TYPE.SPECIAL_SKILL,1);
         this.targetType=TARGET_TYPE.ENEMIES;
+    }
+    public void execute(Combatant actor, List<Combatant> targets, BattleContext battleContext, boolean resetCooldown)
+    {
+        this.execute(actor,targets,battleContext);
+        if (resetCooldown)
+        {
+            actor.resetCooldown();
+        }
     }
 }
